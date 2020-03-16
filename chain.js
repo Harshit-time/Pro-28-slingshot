844fbadcc6c3d6557dd429cffa988842f8e16756
Harshit-time <59550081+Harshit-time@users.noreply.github.com>
2020-03-16 20:24:33 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,32 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+       var options={
+           bodyA: bodyA,
+           pointB: pointB,
+           stiffness: 0.03,
+           length: 8
+       } 
+       this.pointB = pointB;
+      this.sling = Constraint.create(options);
+      World.add(world,this.sling);
+
+    };
+    display(){
+        if(this.sling.bodyA){
+        var posA = this.sling.bodyA.position;
+        var pointB = this.pointB; 
+        
+        
+        strokeWeight(5);
+
+        line(posA.x,posA.y,pointB.x,pointB.y);
+    }}
+    
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    attach(bodyA){
+        this.sling.bodyA = bodyA;
+    }
+}
